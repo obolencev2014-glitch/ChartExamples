4e9f889105d40fff698d5f3c43e9abf174a6ac12
obolencev2014-glitch <obolencev2014@gmail.com>
2025-12-18 21:20:54 +0300

    Add calculator file

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# Simple Tkinter window example
+# Author: <ваше имя>
+# Date: <сегодняшняя дата>
+
+import tkinter as tk
+
+# creating main window
+window = tk.Tk()
+window.geometry("300x200")
+window.title("Python калькулятор")
+
+# start main loop
+window.mainloop()
